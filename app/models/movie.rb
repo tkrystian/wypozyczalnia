5e1bc4551bca29e8b434ab69367a267e83827dcd
GrzegorzTrybula <grzesiek.trybula@gmail.com>
2015-02-17 22:11:31 +0100

    znaczek koszyka nie zadziala na heroku, fix w paperclipie do poprawnej sciezki do amazon s3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,15 @@
 class Movie < ActiveRecord::Base
-  has_attached_file :photo
-  has_attached_file :video
-  validates_attachment_content_type :photo, :video,  :url =>':s3_domain_url',
-                                    :path => '/:class/:attachment/:id_partition/:style/:filename',
-                                    :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :photo, :url =>':s3_domain_url',
+                    :path => '/:class/:attachment/:id_partition/:style/:filename'
 
-  validates_attachment_content_type :video,  :url =>':s3_domain_url',
-                                     :path => '/:class/:attachment/:id_partition/:style/:filename',
-                                     :content_type => ["video/mp4"]
+  has_attached_file :video,
+                    :url =>':s3_domain_url',
+                    :path => '/:class/:attachment/:id_partition/:style/:filename'
+
+  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  validates_attachment_content_type :video,       :content_type => ["video/mp4"]
+
   has_many :orders
   has_many :users, through: :orders
   has_many :reviews
