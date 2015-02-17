6769cf4c2236b73082bb6084f6eb4f9a964c0d1a
Bartek Skrobek <--add>
2015-02-17 19:59:00 +0100

    dodanie linijki has_attached_file do movie dla video

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,10 @@
 class Movie < ActiveRecord::Base
   has_attached_file :photo
+  has_attached_file :video
   validates_attachment_content_type :photo, :video,  :url =>':s3_domain_url',
                                     :path => '/:class/:attachment/:id_partition/:style/:filename',
                                     :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  
+
   validates_attachment_content_typec :video,  :url =>':s3_domain_url',
                                      :path => '/:class/:attachment/:id_partition/:style/:filename',
                                      :content_type => ["video/mp4"]
