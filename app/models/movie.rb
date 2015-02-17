51cdf987e0f26f3d02aa7a998c01ed9e007cdd85
Bartek Skrobek <--add>
2015-02-17 19:55:20 +0100

    zamiana has_attachment_file na validates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,11 +3,13 @@ class Movie < ActiveRecord::Base
   validates_attachment_content_type :photo, :video,  :url =>':s3_domain_url',
                                     :path => '/:class/:attachment/:id_partition/:style/:filename',
                                     :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  
+  validates_attachment_content_typec :video,  :url =>':s3_domain_url',
+                                     :path => '/:class/:attachment/:id_partition/:style/:filename',
+                                     :content_type => ["video/mp4"]
   has_many :orders
   has_many :users, through: :orders
   has_many :reviews
 
-  has_attached_file :video,  :url =>':s3_domain_url',
-                    :path => '/:class/:attachment/:id_partition/:style/:filename',
-                    :content_type => ["video/mp4"]
+
 end
