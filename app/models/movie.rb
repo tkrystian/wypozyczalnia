ebb25b489a9ecefb9787cc9446d12060e67d21c9
Krystian Tarasek <t.krystian@gmail.com>
2015-02-19 21:36:48 +0100

    podmiana gwiazdek i koszyka

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
   has_attached_file :photo, :url =>':s3_domain_url',
                     :path => '/:class/:attachment/:id_partition/:style/:filename'
 
